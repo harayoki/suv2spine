d34f1f76ece0851320b05ee490bb3489c2f26ef0
harayoki <github@harayoki.net>
2017-04-30 23:48:06 +0900

    デフォルトスケール値の設定が可能に

--- suv2spine.py
+++ suv2spine.py
@@ -7,12 +7,16 @@ import argparse
 import math
 import json
 
+VERSION = 1.01
+DEFAULT_SCALE = 10.0
+
 # 書き出しJSONテンプレート Spine3.5.51で簡易形式(non-essential)で書き出したものを元に作成
 # hash値はかぶってもインポート時に別の元して取り込めるので固定でも大丈夫ぽい
-# hull値はなくても大丈夫なようだが、ちゃんと設定するのは大変 データのソートも必要かも
+# hull値とedge値はなくてもとりあえずは大丈夫 (インポート時に警告は出る)
 # @see http://ja.esotericsoftware.com/spine-json-format
 # (hash: A hash of all the skeleton data. This can be used by tools to detect if the data has changed since the last time it was loaded.)
 # (hull: The number of vertices that make up the polygon hull. The hull vertices are always first in the vertices list.)
+# (edges: A list of vertex index pairs that define the edges between connected vertices. Nonessential.)
 output_template = """\
 {
 "skeleton": { "hash": "%(HASH)s", "spine": "%(DATA_VERSION)s", "width": 0, "height": 0 },
@@ -30,7 +34,8 @@ output_template = """\
 				"uvs": %(UVS)s,
 				"triangles": %(TRIANGLES)s,
 				"vertices": %(VERTICES)s,
-				"hull": %(HULL)s
+				"hull": %(HULL)s,
+				"edges": %(EDGES)s
 			}
 		}
 	}
@@ -42,7 +47,7 @@ output_template = """\
 """
 
 # jsonのコンバート
-def convert(suv_json, hash, data_version):
+def convert(suv_json, hash, data_version, scale):
 	mesh = suv_json['mesh'][0]
 	tri = mesh['tri']
 	v2 = mesh['v2']
@@ -58,11 +63,19 @@ def convert(suv_json, hash, data_version):
 	# slot名決め打ち
 	slot = 'slot1'
 	
-	#上下反転
+	# メッシュ拡大
+	for i in range(0, len(v2)):
+		v2[i] *= scale
+	
+	# UV上下反転
 	for i in range(1, len(uv), 2): # 偶数番目＝v座標のみ変換
 		uv[i] = 1.0 - uv[i] 
 	
-	hull = 0 #len(tri) / 3 #temp
+	# hull値(外周の頂点数)
+	hull = 0 # 外周が判定できないので0とする(外周の頂点はverticesの先頭に並んでいなくてはならない)
+	
+	# edge (外周のライン情報 ２頂点データで11ラインを示す)
+	edge = '[]' #とりあえず設定しない
 
 	output_str = output_template %{\
 		'DATA_VERSION': data_version,\
@@ -72,14 +85,15 @@ def convert(suv_json, hash, data_version):
 		'TRIANGLES': tri, \
 		'VERTICES': v2, \
 		'IMAGE': image_name, \
-		'HULL': hull}
+		'HULL': hull, \
+		'EDGES': edge}
 	
 	# 変換後の文字列からjson生成
 	output_json = json.loads(output_str)
 	return output_json
 
 
-parser = argparse.ArgumentParser(description=u'このプログラムの使い方')
+parser = argparse.ArgumentParser(description=u'suv2spine ver. %6.2f 使い方' % VERSION)
 parser.add_argument('file', \
 	action='store', \
 	nargs='?', \
@@ -98,6 +112,16 @@ parser.add_argument('-o', '--out', \
 	type=str, \
 	choices=None, \
 	help=u'アウトプットファイルのパス', \
+	metavar=None)
+
+parser.add_argument('-s', '--scale', \
+	action='store', \
+	nargs=None, \
+	const=None, \
+	default=10.0, \
+	type=float, \
+	choices=None, \
+	help=u'拡大サイズ(デフォルト%6.1f)' % DEFAULT_SCALE, \
 	metavar=None)
 
 parser.add_argument('-dv', '--data_version', \
@@ -125,6 +149,7 @@ file_in = args.file
 file_out = args.out
 data_version = args.data_version
 hash = args.hash
+scale =args.scale
 
 # デフォルトアウトプット名の作成 path/to/json/hoge.json -> path/to/json/hoge_out.json
 if file_out is None: 
@@ -144,15 +169,13 @@ if not os.path.isfile(file_in):
 	quit()
 
 
-
-
 # ファイル読み込み
 fh = open(file_in, 'r')
 suv_json = json.load(fh)
 fh.close()
 
 # データコンバート
-output_json = convert(suv_json, hash, data_version)
+output_json = convert(suv_json, hash, data_version, scale)
 
 # ファイル書き出し
 fh = open(file_out, 'w')
